3733cc81f37f602c5c03f19f0e2cfadf1da8714b
varunkh16 <varun.kh16@gmail.com>
2018-09-30 00:12:33 +0530

    Committing the changes to git

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 const bodyParser = require('body-parser');
 
 var session = new Array();
+var activeReq =new Array();
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -14,8 +15,10 @@ app.all('/process/*', function(req, res) {
 
     var randSec = randomIntFromInterval(15, 20);
 
+    var timeStamp = new Date();
+
     var resJSON = {
-        "time": new Date(),
+        "time": timeStamp,
         "method": req.method,
         "headers": req.headers,
         "path": req._parsedUrl.path,
@@ -27,10 +30,25 @@ app.all('/process/*', function(req, res) {
         resJSON["body"] = req.body;
     }
 
+    activeReq.push(resJSON);
+
+    var id=timeStamp+"|"+req.method;
     setTimeout(function() {
+      var val=id.split("|");
+      for(var a=0;a<activeReq.length;a++){
+        //console.log(activeReq[a].time+"**********"+val[0]);
+        if(activeReq[a].time==val[0]){
+          if(activeReq[a].method==val[1]){
+            console.log(val[0]+"**********"+val[1]);
+            console.log("popping the element "+activeReq[a]+" from array "+activeReq);
+            activeReq.splice(a,1);
+            console.log("popping the element "+activeReq[a]+" from array "+activeReq);
+          }
+        }
+      }
         session.push(resJSON);
         res.send(JSON.stringify(resJSON));
-    }, randSec * 1000);
+    }, randSec * 1000,id);
 
 });
 
@@ -39,6 +57,7 @@ app.all('/stats', function(req, res) {
     var resJSON = {};
 
     resJSON["overall"] = overallAPICompute();
+    resJSON["active requests"] = getActiveRequests();
     resJSON["one hour"] = lastHourAPIData();
     resJSON["one minute"] = lastMinuteAPIData();
 
@@ -56,8 +75,10 @@ function overallAPICompute() {
     if (session.length > 0) {
         var getTotalTime = 0;
         var postTotalTime = 0;
+        var putTotalTime = 0;
         var getCount = 0;
         var postCount = 0;
+        var putCount = 0;
         for (var i = 0; i < session.length; i++) {
             if (session[i].method == "GET") {
                 getTotalTime += session[i].duration;
@@ -67,6 +88,10 @@ function overallAPICompute() {
                 postTotalTime += session[i].duration;
                 postCount++
             }
+            if (session[i].method == "PUT") {
+              putTotalTime += session[i].duration;
+              putCount++
+          }
         }
         if (getCount > 0) {
             resStat["GET"] = getTotalTime / getCount;
@@ -74,8 +99,49 @@ function overallAPICompute() {
         if (postCount > 0) {
             resStat["POST"] = postTotalTime / postCount;
         }
+        if (putCount > 0) {
+          resStat["PUT"] = putTotalTime / putCount;
+      }
     }
     return resStat;
+}
+
+function getActiveRequests(){
+  var resStat = {
+    "total": activeReq.length
+}
+if (activeReq.length > 0) {
+    var getTotalTime = 0;
+    var postTotalTime = 0;
+    var putTotalTime = 0;
+    var getCount = 0;
+    var postCount = 0;
+    var putCount = 0;
+    for (var i = 0; i < activeReq.length; i++) {
+        if (activeReq[i].method == "GET") {
+            getTotalTime += activeReq[i].duration;
+            getCount++;
+        }
+        if (activeReq[i].method == "POST") {
+            postTotalTime += activeReq[i].duration;
+            postCount++
+        }
+        if (activeReq[i].method == "PUT") {
+          putTotalTime += activeReq[i].duration;
+          putCount++
+      }
+    }
+    if (getCount > 0) {
+        resStat["GET"] = getTotalTime / getCount;
+    }
+    if (postCount > 0) {
+        resStat["POST"] = postTotalTime / postCount;
+    }
+    if (putCount > 0) {
+      resStat["PUT"] = putTotalTime / putCount;
+  }
+}
+return resStat;
 }
 
 function lastHourAPIData() {
@@ -87,7 +153,6 @@ function lastHourAPIData() {
     var getCount = 0;
     var postCount = 0;
     for (var i = 0; i < session.length; i++) {
-        totalCount++;
         const start = session[i].time.getTime();
         const end = currentDate.getTime();
 
@@ -98,10 +163,12 @@ function lastHourAPIData() {
         if (seconds <= 3600) {
             if (session[i].method == "GET") {
                 getTotalTime += session[i].duration;
+                totalCount++;
                 getCount++;
             }
             if (session[i].method == "POST") {
                 postTotalTime += session[i].duration;
+                totalCount++;
                 postCount++
             }
         }
